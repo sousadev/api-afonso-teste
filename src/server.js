801e2535e2510ad321bac7ec76fd98431d668cbf
Italo Sousa <italo@italosousa.com.br>
2022-02-16 10:06:23 -0300

    add flag text

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ routes.get('/api', (req, res) => {
       'Design Systems',
       'Voice, Chatbots & Multimodal UI',
     ],
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   });
 });
 
